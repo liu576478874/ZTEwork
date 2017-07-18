b5b8816bd1802eb3414c3ee77c9a9ac5ac9a79d2
liuyuan <576478874@qq.com>
2017-07-18 22:23:01 +0800

    'merge'

--- New.c
+++ New.c
@@ -10,5 +10,11 @@ printf("hello world\n");
 printf("hello world\n");
 printf("hello world\n");
 printf("hello world\n");
+<<<<<<< HEAD
+=======
+printf("hello world\n");
+printf("hello world\n");
+printf("hello world\n");
+>>>>>>> fa8acac... the apple is red
 return 0;
 }
